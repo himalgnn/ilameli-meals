5ad3c85bf887e5543485d83cf0968daf064f5ea5
Himal Guragain <96119541+himalgnn@users.noreply.github.com>
2022-07-20 17:14:56 +0545

    Actual Project Website

---
+++ Website/js/script.js
@@ -0,0 +1,68 @@
+// Variables
+let navbar_ul = document.getElementById("navbar-ul");
+let logo = document.getElementById("logo");
+let open_menu = document.getElementById("open-menu");
+let welcome = document.getElementById("welcome");
+let services_container = document.getElementById("services-container");
+let delivery_container = document.getElementById("delivery-container");
+let team_container = document.getElementById("team-container");
+let contact = document.getElementById("contact");
+let footer = document.getElementById("footer");
+
+// Function Open Mobile Menu 
+function openMenu() {
+    navbar_ul.style.left = "0%";
+
+    // Blur Elements on mobile menu toggle
+    logo.style.filter = "blur(4px)";
+    open_menu.style.filter = "blur(4px)";
+    welcome.style.filter = "blur(4px)";
+    services_container.style.filter = "blur(4px)";
+    delivery_container.style.filter = "blur(4px)";
+    team_container.style.filter = "blur(4px)";
+    contact.style.filter = "blur(4px)";
+    footer.style.filter = "blur(4px)";
+}
+
+// Function Close Mobile Menu 
+function closeMenu() {
+    navbar_ul.style.left = "-100%";
+
+    // UnBlur Elements on mobile menu toggle off
+    logo.style.filter = "blur(0)";
+    open_menu.style.filter = "blur(0)";
+    welcome.style.filter = "blur(0)";
+    services_container.style.filter = "blur(0)";
+    delivery_container.style.filter = "blur(0)";
+    team_container.style.filter = "blur(0)";
+    contact.style.filter = "blur(0)";
+    footer.style.filter = "blur(0)";
+
+}
+
+// Remove Mobile Menu on devices with width greater than 950px starts
+
+    // Function Definition & Logic
+    function remove_menu(media_width) {
+        if (media_width.matches) {
+            closeMenu();
+        }
+    }
+    // Variable for defining media query width
+    let media_width = window.matchMedia("(min-width:950px)");
+
+    // Function call
+    remove_menu(media_width);
+
+    // Attach listener function on state changes
+    media_width.addListener(remove_menu);
+
+// Remove Mobile Menu on devices with width greater than 950px ends
+
+
+// Remove mobile menu when escape key pressed
+    window.addEventListener('keydown', function(event){
+        if(event.key=="Escape"){
+            closeMenu();
+        }
+    });
